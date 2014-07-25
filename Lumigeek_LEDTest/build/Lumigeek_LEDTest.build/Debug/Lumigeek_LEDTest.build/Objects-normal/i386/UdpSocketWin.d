b7d7e5b24ddf621c502adf818916420ea25cd001
Jack Kalish <jack@jackkalish.com>
2014-07-25 12:33:23 -0700

    removed irnored files

--- Lumigeek_LEDTest/build/Lumigeek_LEDTest.build/Debug/Lumigeek_LEDTest.build/Objects-normal/i386/UdpSocketWin.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Volumes/HDD/OpenFrameworks/of_v0.8.3_osx_release/addons/ofxOsc/libs/oscpack/src/ip/win32/UdpSocketWin.cpp \
-  ../../../addons/ofxOsc/libs/oscpack/src/ip/UdpSocket.h \
-  ../../../addons/ofxOsc/libs/oscpack/src/ip/NetworkingUtils.h \
-  ../../../addons/ofxOsc/libs/oscpack/src/ip/IpEndpointName.h
