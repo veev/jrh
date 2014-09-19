b049356930e4b789814336bb04abc7f520c107bc
Jack Kalish <jack@floating.pt>
2014-09-19 11:48:21 -0700

    settings and cleanup

--- JRHMemorialWall/src/ofApp.cpp
+++ JRHMemorialWall/src/ofApp.cpp
@@ -33,7 +33,7 @@ void ofApp::setup(){
     DataManager::settings.pushTag("waves");
     for(int i=0; i < DataManager::settings.getNumTags("wave"); i++){
         string type = DataManager::settings.getAttribute("wave", "type", "", i);
-        cout<<"making a wave of type: " << type << endl;
+        //cout<<"making a wave of type: " << type << endl;
         int x = DataManager::settings.getAttribute("wave", "x", 0, i);
         int y = DataManager::settings.getAttribute("wave", "y", 0, i);
         int w = DataManager::settings.getAttribute("wave", "width", 0, i);
